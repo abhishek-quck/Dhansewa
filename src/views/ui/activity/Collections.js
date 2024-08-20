637fb4f9c876eaa8b3106b2d999d06eb9efe590c
Abhishek <absk1901mff@gmail.com>
2024-08-20 10:52:35 +0530

    +collections

--- src/views/ui/activity/Collections.js
+++ src/views/ui/activity/Collections.js
@@ -1,7 +1,32 @@
-import React from 'react'
-import {  Card, CardBody, CardHeader, Col, Row, Table } from 'reactstrap';
+import React, { useEffect, useState } from 'react'
+import {  Button, Card, CardBody, CardHeader, Col, Row, Table } from 'reactstrap';
 
 function Collections() {
+    const [collections, setCollections] = useState([
+        { 
+            branch: 'Jacob',
+            date: 'Thornton',
+            dues : 'Thornton',
+            collected: 'Thornton',
+            center: 'Thornton',
+            client: 'fat',
+            DBC: 'fat',
+            disb: 'fat',  
+        },
+        {   
+            branch: 'Jacob',
+            date: 'Thornton',
+            dues : 'Thornton',
+            collected: 'Thornton',
+            center: 'Thornton',
+            client: 'fat',
+            DBC: 'fat',
+            disb: 'fat' 
+        }
+    ]);
+    useEffect(()=>{
+
+    },[])
 
     return (
         <Row>
@@ -27,30 +52,22 @@ function Collections() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>Thornton</td>
-                    <td>Thornton</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>@fat</td>
-                    <td>@fat</td>
-                    <td>@mdo</td>
-                    </tr> 
+                    {collections.map( (row,i) => {
+                        return (
+                            <tr key={i}>
+                                <td>{i+1}</td>                    
+                                <td>{row.branch}</td>
+                                <td>{row.date}</td>
+                                <td>{row.dues}</td>
+                                <td>{row.collected}</td>
+                                <td>{row.center}</td>
+                                <td>{row.client}</td>
+                                <td>{row.DBC}</td>
+                                <td>{row.disb}</td>
+                                <td><Button type='button' color='primary'> View Center </Button></td>
+                            </tr>
+                        )
+                    })} 
                 </tbody>
                 <tfoot>
                    <tr>
