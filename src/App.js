078e08a638f7d860031c825153127f6a0b4f1ffa
Abhishek <ajayquckit@gmail.com>
2024-08-28 18:27:54 +0530

    +css datepicker

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useSelector } from "react-redux"; 
 import ShowError from "./components/errorHandling/ShowError";
+import "react-datepicker/dist/react-datepicker.css";
 
 axios.defaults.baseURL='https://demoapi.dhanseva.co.in/public/api';
 // axios.defaults.baseURL='http://localhost/Dhansewa-API/public/api';
