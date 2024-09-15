edba21b858f91f9fd310ae3cd7b2717b5a5d9752
Abhishek <absk1901mff@gmail.com>
2024-09-16 03:15:49 +0530

    something went wrong last time

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import { useSelector } from "react-redux";
 import ShowError from "./components/errorHandling/ShowError";
 import "react-datepicker/dist/react-datepicker.css";
 
-// axios.defaults.baseURL='https://demoapi.dhanseva.co.in/public/api';
-axios.defaults.baseURL='http://localhost/Dhansewa-API/public/api';
+axios.defaults.baseURL='https://demoapi.dhanseva.co.in/public/api';
+// axios.defaults.baseURL='http://localhost/Dhansewa-API/public/api';
 axios.defaults.headers.common = {
   "Accept":"application/json",
   "Content-Type":"application/json", 
